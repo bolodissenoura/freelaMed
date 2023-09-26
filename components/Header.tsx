1836483afdc8d09dc9cd5f4a599897cc0707241c
daniellima <Daniel.Silva.ext@cyrela.com.br>
2023-09-26 17:08:05 -0300

    feat: header zindex

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,20 @@ const Header = () => {
   return (
     <header
       id="header"
-      className="bg-white rounded-b-3xl flex justify-center fixed w-full">
+      className="bg-white rounded-b-3xl flex justify-center fixed w-full"
+      style={{ zIndex: 99 }}>
       <div className="header-container relative py-4 justify-between md:justify-start flex items-center">
         <div className="flex items-center mr-8">
-          <h1>
-            <Link href="/" className="logo">
-              <Image
-                src="/icons/freelamed-logo-dark.png"
-                alt="logo"
-                className="h-6 w-auto"
-                height="25"
-                width="100"
-                priority
-              />
-            </Link>
-          </h1>
+          <Link href="/" className="logo">
+            <Image
+              src="/icons/freelamed-logo-dark.png"
+              alt="logo"
+              className="h-6 w-auto"
+              height="25"
+              width="100"
+              priority
+            />
+          </Link>
         </div>
       </div>
     </header>
